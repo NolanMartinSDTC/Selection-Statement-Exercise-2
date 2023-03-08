61303c7403ee2a898d260f29d71b93690d3c8d5d
Nolan Martin <nmartinsdtc@gmail.com>
2023-03-08 11:15:55 -0700

    code for exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What's your favorite school subject?");
+            string favSubject = (Console.ReadLine().ToLower());
+
+            switch (favSubject)
+            {
+                case "math":
+                    Console.WriteLine("Math is fun!");
+                    break;
+
+                case "science":
+                    Console.WriteLine("Science is cool!");
+                    break;
+
+                case "art":
+                    Console.WriteLine("Art is beautiful!");
+                    break;
+
+                case "english":
+                    Console.WriteLine("English is great! I'm not good at it though :(");
+                    break;
+
+                case "music":
+                    Console.WriteLine("I cant play music, but I love to listen!");
+                    break;
+
+                default:
+                    Console.WriteLine("I'm not familiar with that subject, but it sounds interesting!");
+                    break;
+            }
         }
     }
 }
